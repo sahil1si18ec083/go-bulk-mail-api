a1a33bfd9bf7829a90e6509a3e4d7f8ef808129d
Sahil kumar <sahil.1si18ec083@gmail.com>
2026-05-13 01:03:57 +0530

    dynamic port

--- main.go
+++ main.go
@@ -44,7 +44,13 @@ func main() {
 
 	r.POST("/send-mail", SendMailHandler)
 
-	r.Run(":8080")
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	r.Run(":" + port)
 }
 
 func SendMailHandler(c *gin.Context) {
